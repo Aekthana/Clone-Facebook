bb34e4e978a69952865f55aac739c8cdaffdeab6
Aekthana <okkratoo@hotmail.com>
2023-04-02 13:49:27 +0700

    start-pj

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+const Header = ()=>{
+    return(
+        <header>
+            <h1>facebook</h1>
+            <form action="">
+                <input type="text" />
+            </form>
+            <div>
+                <div><i></i></div>
+                <div><i></i></div>
+                <div><i></i></div>
+            </div>
+        </header>
+    )
+}
+export default Header
