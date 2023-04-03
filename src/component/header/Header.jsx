7a55f65a5ee0bbf3613cc23dff42c55cebd2ae23
Aekthana <okkratoo@hotmail.com>
2023-04-03 15:26:44 +0700

    start-header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,10 +2,10 @@
 
 const Header = ()=>{
     return(
-        <header>
-            <h1>facebook</h1>
+        <header className="min-w-full flex justify-between items-center pr-3 pl-3 pt-2 pb-2  bg-green-400">
+            <h3 className=" text-slate-950 text-xl font-bold ">facebook</h3>         
             <form action="">
-                <input type="text" />
+                <input type="text" placeholder="ค้นหาบน Facebook:"/>
             </form>
             <div>
                 <div><i></i></div>
