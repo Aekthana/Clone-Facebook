f35111668da91c2f93867335a82aeccf8918dcb4
Aekthana <okkratoo@hotmail.com>
2023-04-17 00:17:08 +0700

    add-API

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import axios from 'axios'
+import { useSelector } from 'react-redux'
 import Aside from './component/Aside'
 import Header from './component/Header'
 import Main from './component/Main'
 import Navbar from './component/Navbar'
-import '/src/scrollstyle.css'
+import '/src/App.css'
+import Profile from './component/Profile'
 
 function App() {
-  
-  axios.get('https://reqres.in/api/users/1')
-  .then(response => {
-    console.log(response.data.data)
-  })
-  .catch(error => {
-    console.log(error)
-  })
+   
+ 
 
   return (
     <div className="App text-gray-900 ">
-      <div className='relative bg-green-500 h-12 z-50'>
+      {/* <div className='relative bg-green-500 h-12 z-50'>
         <Header></Header>
       </div>
       <div className=' flex justify-center relative w-full h-screen bg-green-500'>
         <Navbar></Navbar>
         <Main></Main>
         <Aside></Aside>
-      </div>
-
+      </div> */}
+      <Profile></Profile>
     </div>
   )
 }
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import profileSlice from "./profileSlice";
+
+
+export default configureStore({
+    reducer:{
+        profileStore : profileSlice
+    }
+})
